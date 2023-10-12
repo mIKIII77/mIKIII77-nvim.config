5ca6a10558c322b8cb12b14b0099522162632f22
mIKIII77 <mikhail.karapetyan@uha.fr>
2023-10-12 19:49:10 +0200

    v.0.0.3
    - Add some mappings for tab management

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -4,5 +4,7 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
 vim.keymap.set('n', '<leader>s', ':Neotree buffers focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
-
-
+vim.keymap.set('n', '<leader>t', ':tabnew<CR>')
+vim.keymap.set('n', '<leader>r', ':tabclose<CR>')
+vim.keymap.set('n', '<leader>é', ':tabnext<CR>')
+vim.keymap.set('n', '<leader>&', ':tabprevious<CR>')
